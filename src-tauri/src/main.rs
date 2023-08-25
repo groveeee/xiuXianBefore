e03a8136b17a467a80b3e839792f28c1b226a0a8
grovee <zgl129king@163.com>
2023-08-25 20:59:44 +0800

    add:调用rust接口示例

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,30 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::time::Duration;
+use std::thread;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[tauri::command]
+/// 这里的参数名字必须和js传入的参数的名字相同
+fn greet(name: &str) -> String {
+    let ten_seconds = Duration::from_secs(10);
+    // Start a new thread to run the task
+    let handle = thread::spawn(move || {
+        loop {
+            // Your task code here
+            println!("Executing task...");
+            // Sleep for 10 seconds
+            thread::sleep(ten_seconds);
+        }
+    });
+    // Wait for the thread to finish
+    handle.join().unwrap();
+    String::from("xxx")
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import Greet from '../lib/Greet.svelte'
 </script>
 
 <svelte:head>
@@ -24,6 +25,8 @@
 	<h2>
 		try editing <strong>src/routes/+page.svelte</strong>
 	</h2>
+	<h1>Welcome to SvelteKit</h1>
+	<Greet />
 
 	<Counter />
 </section>
